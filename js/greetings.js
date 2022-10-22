196faf03cdbc4caa1bfae2e56770ef9eb70a8e67
luls.me <luis.me>
2022-10-22 11:12:01 +0900

    로그인 수정

--- js/greetings.js
+++ js/greetings.js
@@ -1,12 +1,39 @@
 const loginForm = document.getElementById('login-form');
-const greetings = document.getElementById('greetings');
+const greetings = document.getElementById('greeting');
 const loginInput = document.getElementById('username');
 
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = 'username';
 
 function onLoginSubmit(event) {
-  event.preventDefault();
+  event.preventDefault();const loginForm = document.querySelector("#login-form");
+  const loginInput = document.querySelector("#login-form input");
+  const greeting = document.querySelector("#greeting");
+  
+  const HIDDEN_CLASSNAME = "hidden";
+  const USERNAME_KEY = "username";
+  
+  function onLoginSubmit(event) {
+    event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreetings(username);
+  }
+  
+  function paintGreetings(username) {
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+  }
+  
+  const savedUsername = localStorage.getItem(USERNAME_KEY);
+  
+  if (savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+  } else {
+    paintGreetings(savedUsername);
+  }
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
   localStorage.setItem(USERNAME_KEY, username);
@@ -14,12 +41,12 @@ function onLoginSubmit(event) {
 }
 
 function paintGreetings(username) {
-  greetings.innerText = `Hello ${username}`;
+  console.info('user', username);
+  greetings.innerHTML = `Hello ${username}`;
   greetings.classList.remove(HIDDEN_CLASSNAME);
 }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
-console.info('save', savedUsername);
 
 if (savedUsername === null) {
   loginForm.classList.remove(HIDDEN_CLASSNAME);
